2646410d8e17e3bd33fe0aa31c276936e79cf31a
Milk <tientd4@mbbank.com.vn>
2025-06-12 23:36:10 +0700

    fix error

--- src/main/java/com/example/DaPhone/Model/LoginResponse.java
+++ src/main/java/com/example/DaPhone/Model/LoginResponse.java
@@ -13,36 +13,13 @@ public class LoginResponse {
 	private String auth;
 	private String username;
 	private Long userId;
+	
+	// Constructor với 2 tham số
+	// a giải thích qua thì cái @AllArgsConstructor nó tự động thay cho việc e khai báo mục get set kia rồi.
+	// nên là e dùng cái @AllArgsConstructor mà còn khai báo get set thì nó sẽ bị lỗi báo là đã có constructor rồi.
 	public LoginResponse(String auth, String username) {
 		super();
 		this.auth = auth;
 		this.username = username;
 	}
-	
-	public LoginResponse(String auth, String username, Long userId) {
-		super();
-		this.auth = auth;
-		this.username = username;
-		this.userId = userId;
-	}
-
-	public String getAuth() {
-		return auth;
-	}
-	public void setAuth(String auth) {
-		this.auth = auth;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public Long getUserId() {
-		return userId;
-	}
-	public void setUserId(Long userId) {
-		this.userId = userId;
-	}
-	
 }
